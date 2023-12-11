56c75b58a8bad8555a9410f091996e1aa7006a42
Lee <ca.y25@hotmail.com>
2023-12-11 14:40:09 -0500

    Add form validation using Zod library

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Little Lemon Meta Front-end Capsone
 Little Lemon Restaurant - Reservation
 
+## Description
+This is a Final Capsone Project for Meta Front-end. This project is a reservation system for a restaurant. The user can make a reservation. The user can also view the reservation history and cancel the reservation.
+
--- src/pages/book/components/BookingForm.jsx
+++ src/pages/book/components/BookingForm.jsx
@@ -1,21 +1,51 @@
 import { useState } from "react";
+import { object, string, date, number, union } from "zod";
+
+const reservationSchema = object({
+  name: string().min(2).max(50),
+  date: date(),
+  time: string(),
+  guests: number().min(1),
+  occasion: union([
+    string().max(100), // Custom occasion
+    string("Birthday", "Anniversary"), // Predefined occasions
+  ]),
+});
+
 const ReservationForm = ({ onSubmit }) => {
-  const [name, setName] = useState('');
-  const [date, setDate] = useState('');
-  const [time, setTime] = useState('');
-  const [guests, setGuests] = useState('');
-  const [occasion, setOccasion] = useState('');
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [guests, setGuests] = useState("");
+  const [occasion, setOccasion] = useState("");
+  const [validationErrors, setValidationErrors] = useState({});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (name && date && time && guests && occasion) {
-      onSubmit({ name, date, time, guests, occasion });
-      // Limpiar los campos después de enviar la reserva
-      setName('');
-      setDate('');
-      setTime('');
-      setGuests('');
-      setOccasion('');
+
+    try {
+      // Validate the form data
+      const data = reservationSchema.parse({
+        name,
+        date,
+        time,
+        guests: parseInt(guests, 10),
+        occasion,
+      });
+
+      // If validation succeeds, submit the data
+      onSubmit(data);
+
+      // Clear the form after submission
+      setName("");
+      setDate("");
+      setTime("");
+      setGuests("");
+      setOccasion("");
+      setValidationErrors({});
+    } catch (error) {
+      // If validation fails, set the validation errors
+      setValidationErrors(error.errors);
     }
   };
 
@@ -25,24 +55,31 @@ const ReservationForm = ({ onSubmit }) => {
       <form onSubmit={handleSubmit}>
         <label>
           Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
         </label>
         <label>
           Date:
-          <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+          <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required/>
         </label>
         <label>
           Time:
-          <input type="time" value={time} onChange={(e) => setTime(e.target.value)} />
+          <input type="time" value={time} onChange={(e) => setTime(e.target.value)} required/>
         </label>
         <label>
           Guests:
-          <input type="number" value={guests} onChange={(e) => setGuests(e.target.value)} />
+          <input type="number" value={guests} onChange={(e) => setGuests(e.target.value)} required/>
         </label>
-        <label className="label-full">
+        <label>
           Occasion:
-          <input type="text" value={occasion} onChange={(e) => setOccasion(e.target.value)} />
+          <select value={occasion} onChange={(e) => setOccasion(e.target.value)} required>
+            <option value="">Select an occasion</option>
+            <option value="Birthday">Birthday</option>
+            <option value="Anniversary">Anniversary</option>
+          </select>
         </label>
+        {validationErrors.occasion && (
+          <p className="validation-error">{validationErrors.occasion}</p>
+        )}
         <button type="btn-lg-secondary submit">Reserve Now</button>
       </form>
     </div>
